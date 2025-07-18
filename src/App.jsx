bce33d3eaec2f2dba64ef0465da5f862ae40728f
NarmathasriS <srinarmatha304@gmail.com>
2025-07-18 11:42:06 +0530

    pathchanges in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
-import Home from "./pages/home";
+import Home from "../src/pages/Home";
 import Cart from "./pages/cart";
 import Shop from "./pages/shop";
 import Contact from "./pages/contact";
